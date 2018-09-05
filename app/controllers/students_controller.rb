259d7c907184c6745e38633180b23f26205625d5
Scott Weinstein <sweinstein27@gmail.com>
2018-09-05 13:30:08 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -6,11 +6,7 @@ class StudentsController < ApplicationController
   end
 
   def show
+    @student = Student.find(params[:id])
   end
 
-  private
-
-    def set_student
-      @student = Student.find(params[:id])
-    end
-end
+  
